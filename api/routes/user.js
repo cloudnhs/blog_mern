e3adba1d8fd202b20d7fa96b25aef1ae5c8a5f61
william <cloudnhs@gmail.com>
2020-07-01 20:32:02 +0900

    structed api

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,15 +2,39 @@ const express = require('express');
 const router = express.Router();
 
 
-
-// @route   GET user/test
-// @desc    Test user route
+// @route   POST user/register
+// @desc    user register
 // @access  Public
 
-router.get('/test', (req, res) => {
-    res.json({message : 'api test'});
+router.post('/register', (req, res) => {
+    res.json({
+        message : 'registed user'
+    })
 })
 
+
+// @route   POST user/login
+// @desc    user login
+// @access  Public
+
+router.post('/login', (req, res) => {
+    res.json({
+        message : 'user login'
+    })
+})
+
+
+
+
+// @route   GET user/current
+// @desc    current user
+// @access  Private
+
+router.get('/current', (req, res) => {
+    res.json({
+        message : 'current user'
+    })
+})
 
 
 
