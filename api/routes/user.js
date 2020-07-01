c1327d3fecc0d828d7065198c674071f6ee9972a
william <cloudnhs@gmail.com>
2020-07-01 20:59:20 +0900

    hash password

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
 
 const userModel = require('../../model/user');
 
@@ -8,26 +10,38 @@ const userModel = require('../../model/user');
 // @access  Public
 
 router.post('/register', (req, res) => {
-    const newUser = new userModel({
-        username : req.body.username,
-        email: req.body.email,
-        password:req.body.password,
-        avatar: req.body.avatar
-    })
 
-    newUser
-        .save()
-        .then(result => {
-            res.json({
-                message : 'successful sign up',
-                userInfo: result
-            })
-        })
-        .catch(err => {
-            res.json({
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+        //password 암호화 실패
+        if(err) {
+            return res.status(500).json({
                 error : err.message
+            })           
+        }else{
+            // password 암호화 성공 후 db 저장
+            const newUser = new userModel({
+                username : req.body.username,
+                email: req.body.email,
+                password:hash,
+                avatar: req.body.avatar
             })
-        })
+        
+            newUser
+                .save()
+                .then(result => {
+                    res.json({
+                        message : 'successful sign up',
+                        userInfo: result
+                    })
+                })
+                .catch(err => {
+                    res.json({
+                        error : err.message
+                    });
+                });
+        }
+    })
+    
     
     // res.json({
     //     message : 'registed user'
